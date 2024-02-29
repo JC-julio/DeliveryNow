1b457b4af86bc231cb9091ad85bdb505003b7158
Júlio <jcaguiarpg@gmail.com>
2024-02-29 19:34:25 -0400

    funcionalidade de enviar emails feita

--- src/application/usecase/RecoverPassword/EmailforUpdatePassword.usecase.ts
+++ src/application/usecase/RecoverPassword/EmailforUpdatePassword.usecase.ts
@@ -5,27 +5,21 @@ config();
 export default class EmailforUpdatePassword {
     constructor() {}
     async execute(props: Input) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.email,
-            pass: process.env.password,
-        }
-    });
+        const transporter = await nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.email,
+                pass: process.env.password,
+            }
+        });
     const mailOptions = {
-        from: transporter.auth.user,
+        from: transporter.transporter.auth.user,
         to: props.email,
         subject: 'Recuperação de senha do DeliveryNow',
         text: 'O código para redefinir sua senha é: ' + props.code
     };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email enviado: ' + info.response);
-        }
-    });
-}
+    return await transporter.sendMail(mailOptions);
+    }
 }
 
 export type Input = {
---
+++ src/application/usecase/test/RecoverPassword/EmailforUpdatePassword.usecase.test.ts
@@ -0,0 +1,10 @@
+import EmailforUpdatePassword from "../../RecoverPassword/EmailforUpdatePassword.usecase"
+
+
+//não foi possivel concluir este teste por completo pois não é possivel verificar
+// se o email chegou, salvo nas ocasiões em que uma tela está aberta ao lado
+
+test("deve enviar um email de verificação", async() => {
+    const usecase = new EmailforUpdatePassword()
+    await usecase.execute({email: 'anapaguiargp@gmail.com', code: '12345678'})
+}, 15000)
