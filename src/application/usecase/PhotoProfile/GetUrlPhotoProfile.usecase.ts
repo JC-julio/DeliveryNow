36a30bb8f094edf85b7f11d14ab5f8e85dafedec
Júlio <jcaguiarpg@gmail.com>
2024-03-01 20:45:07 -0400

    atualização de otimização no caso de uso para pegar a url da imagem no BD

--- src/application/usecase/PhotoProfile/GetUrlPhotoProfile.usecase.ts
+++ src/application/usecase/PhotoProfile/GetUrlPhotoProfile.usecase.ts
@@ -5,9 +5,9 @@ import DeliveryMan from "src/domain/DeliveryMan";
 export default class PhotoProfile {
     constructor(readonly repo: PhotoProfileRepositoryInterface) {}
     async execute(props: Input): Promise<Output> {
-        if(!await this.repo.GetOne(props.id))
-            throw new Error("usuário não encontrado")
         const user = await this.repo.GetUrlPhotoProfile(props.id)
+        if(!user || user.URLPhotoProfile)
+            throw new Error("usuário não encontrado")
         return {
             URLPhotoProfile: user.URLPhotoProfile
         }
